0d2265578408defd74f2c0f354aa7159524e2da9
Maarit Laakso <maarit.laakso2@myy.haaga-helia.fi>
2023-02-08 17:48:29 +0200

    trying to solve routing issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import FrontPage from "./Frontpage/FrontPage";
 import ResponsiveAppBar from "./Components/ResponsiveAppBar";
 import Stations from "./Stations/Stations";
@@ -10,16 +10,16 @@ import UserExperiences from "./UserExperiences/UserExperiences";
 function App() {
 
   return (
-
-    <BrowserRouter > 
+    <HashRouter basename ={process.env.PUBLIC_URL}> 
+      console.log(basename)
       <ResponsiveAppBar />
       <Routes >
         <Route path="/" exact element={<FrontPage />} />
-        <Route path={`/stations`} exact element={<Stations />} />
-          <Route path={`/journeys`} exact element={<Journeys />} />
-          <Route path={`/userexperiences`} exact element={<UserExperiences />} />     
+        <Route path="/stations" exact element={<Stations />} />
+          <Route path="/journeys" exact element={<Journeys />} />
+          <Route path="/userexperiences" exact element={<UserExperiences />} />     
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default App;
